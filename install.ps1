f8e8cd58129b6e0f0c5d128d7ac147285bc4458c
Tim Schneider <timschneider.xyz@gmail.com>
2021-06-14 00:11:37 +0200

    formatting

--- install.ps1
+++ install.ps1
@@ -1,25 +1,25 @@
 #Requires -RunAsAdministrator
 Write-Host -ForegroundColor Cyan @"
 
-                   888          888     .d888 d8b 888
-                   888          888    d88P"  Y8P 888
-                   888          888    888        888
-               .d88888  .d88b.  888888 888888 888 888  .d88b.  .d8888b
-              d88" 888 d88""88b 888    888    888 888 d8P  Y8b 88K
-              888  888 888  888 888    888    888 888 88888888 "Y8888b.
-          d8b Y88b 888 Y88..88P Y88b.  888    888 888 Y8b.          X88
-          Y8P  "Y88888  "Y88P"   "Y888 888    888 888  "Y8888   88888P'
+         888          888     .d888 d8b 888
+         888          888    d88P"  Y8P 888
+         888          888    888        888
+     .d88888  .d88b.  888888 888888 888 888  .d88b.  .d8888b
+    d88" 888 d88""88b 888    888    888 888 d8P  Y8b 88K
+    888  888 888  888 888    888    888 888 88888888 "Y8888b.
+d8b Y88b 888 Y88..88P Y88b.  888    888 888 Y8b.          X88
+Y8P  "Y88888  "Y88P"   "Y888 888    888 888  "Y8888   88888P'
 
 "@
 Write-Host -ForegroundColor Yellow @"
 
-          -------------------------------------------------------------
+------------------------------------------------------------
 
-          Author     : Tim Schneider
-          Website    : https://timschneider.xyz
-          Repository : https://github.com/RanzigeButter/dotfiles
+Author     : Tim Schneider
+Website    : https://timschneider.xyz
+Repository : https://github.com/RanzigeButter/dotfiles
 
-          -------------------------------------------------------------
+------------------------------------------------------------
 
 
 "@
@@ -61,14 +61,14 @@ Remove-Item $dotfiles -Recurse -Force
 Write-Host -ForegroundColor Yellow @"
 
 
-          -------------------------------------------------------------
+------------------------------------------------------------
 
-          Congrats!
+Congrats!
 
-          Provided there were no hidden errors, the script completed
-          successfully and everything should be in place.
+Provided there were no hidden errors, the script completed
+successfully and everything should be in place.
 
-          -------------------------------------------------------------
+------------------------------------------------------------
 
 
 "@
--- update.ps1
+++ update.ps1
@@ -1,11 +1,10 @@
 Write-Host -ForegroundColor Yellow @"
 
-          -------------------------------------------------------------
+--------------------------------------------------
 
-          Start updating dotfiles...
+Updating dotfiles...
 
-          -------------------------------------------------------------
-
+--------------------------------------------------
 
 "@
 
@@ -33,40 +32,27 @@ Write-Host " Done"
 
 # PowerShell Profile
 Write-Host "Updating PowerShell profile..." -NoNewline
-if (!(Test-Path "$env:USERPROFILE\Documents\WindowsPowerShell")) {
-  New-Item "$env:USERPROFILE\Documents\WindowsPowerShell" -ItemType Directory | Out-Null
-}
 Copy-Item "$dotfiles\powershell\profile.ps1" "$env:USERPROFILE\Documents\WindowsPowerShell"
 Write-Host " Done"
 
 # Windows Terminal
+Write-Host "Updating Windows Terminal settings..." -NoNewline
 $folderWT = Get-ChildItem "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_*" | Select-Object -First 1 -Expand FullName
-if ($folderWT) {
-  Write-Host "Updating Windows Terminal settings..." -NoNewline
-  Copy-Item "$dotfiles\terminal\settings.json" "$folderWT\LocalState"
-  Write-Host " Done"
-}
+Copy-Item "$dotfiles\terminal\settings.json" "$folderWT\LocalState"
+Write-Host " Done"
 
 # VS Code
-if (Get-Command code -ea 0) {
-  Write-Host "Updating VS Code settings..." -NoNewline
-  Copy-Item "$dotfiles\vscode\settings.json" "$env:APPDATA\Code\User"
-  Write-Host " Done"
-}
+Write-Host "Updating VS Code settings..." -NoNewline
+Copy-Item "$dotfiles\vscode\settings.json" "$env:APPDATA\Code\User"
+Write-Host " Done"
 
 # SSH
 Write-Host "Updating SSH config..." -NoNewline
-if (!(Test-Path "$env:USERPROFILE\.ssh")) {
-  New-Item "$env:USERPROFILE\.ssh" -ItemType Directory | Out-Null
-}
 Copy-Item "$dotfiles\ssh\config" "$env:USERPROFILE\.ssh"
 Write-Host " Done"
 
 # Git
 Write-Host "Updating Git config..." -NoNewline
-if (!(Test-Path "$env:USERPROFILE\.config\git")) {
-  New-Item "$env:USERPROFILE\.config\git" -ItemType Directory | Out-Null
-}
 Copy-Item "$dotfiles\git\config" "$env:USERPROFILE\.config\git"
 Write-Host " Done"
 
@@ -76,11 +62,10 @@ Write-Host " Done"
 Remove-Item $dotfiles -Recurse -Force
 Write-Host -ForegroundColor Yellow @"
 
+--------------------------------------------------
 
-          -------------------------------------------------------------
+Congrats, you are now up to date!
 
-          Congrats, you are now up to date!
-
-          -------------------------------------------------------------
+--------------------------------------------------
 
 "@
