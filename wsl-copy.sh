30cea2711fd3c6a8ecbd4d8943fe48db85b9b3a1
Tim Schneider <timschneider.xyz@gmail.com>
2022-02-26 13:51:24 +0100

    switch to wsl - copy

---
+++ wsl-copy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#                              _
+#   ___ ___  _ __  _   _   ___| |__
+#  / __/ _ \| '_ \| | | | / __| '_ \
+# | (_| (_) | |_) | |_| |_\__ \ | | |
+#  \___\___/| .__/ \__, (_)___/_| |_|
+#           |_|    |___/
+
+
+git clone https://github.com/RanzigeButter/dotfiles.git
+cp -r dotfiles/wsl/. ~/
+rm -rf dotfiles
+source ~/.zshenv
+source ~/.config/zsh/.zshrc
