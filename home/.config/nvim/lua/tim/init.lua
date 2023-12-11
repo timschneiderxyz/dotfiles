65a3514e0a6311c006460067523a0cdbdce5730d
Tim Schneider <timschneider.xyz@gmail.com>
2023-12-11 21:57:32 +0100

    added neovim config

---
+++ home/.config/nvim/lua/tim/init.lua
@@ -0,0 +1,24 @@
+-- General
+vim.api.nvim_exec('language en_US', false)
+-- vim.opt.termguicolors = true
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv('XDG_CACHE_HOME') .. '/nvim/undo'
+vim.opt.clipboard = 'unnamedplus'
+
+-- Editor
+vim.opt.signcolumn = 'yes'
+vim.opt.number = true
+vim.opt.colorcolumn = '80'
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Indenting
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
