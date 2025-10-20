d51a435769653141b82c07c9531bf30d78d7b5a1
Tim Schneider <timschneider.xyz@gmail.com>
2025-10-20 10:48:09 +0200

    updated

---
+++ files/home/.config/nvim/lua/plugins/mini-pairs.lua
@@ -0,0 +1,10 @@
+return {
+  "nvim-mini/mini.pairs",
+  opts = {
+    modes = { insert = true, command = true, terminal = false },
+    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    skip_ts = { "string" },
+    skip_unbalanced = true,
+    markdown = true
+  }
+}
--- files/home/.config/nvim/lua/plugins/which-key.lua
+++ files/home/.config/nvim/lua/plugins/which-key.lua
@@ -1,8 +1,7 @@
 return {
   "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup {
-      delay = 1000
-    }
-  end
+  event = "VeryLazy",
+  opts = {
+    delay = 1000
+  }
 }
